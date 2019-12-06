507479b2197d724ffc86497232287101545e98f8
bamboo110 <9bbambangsetyo.b@gmail.com>
2019-12-06 22:15:09 +0700

    last commit

--- script.js
+++ script.js
@@ -4,29 +4,54 @@ const card = document.querySelectorAll('.card');
 // button
 const button = document.querySelectorAll('.bar');
 
-// counter
-let counter = 1;
+// width
 const size = card[0].clientWidth;
 
-slide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+slide.style.transform = 'translateX(' + (-size * 1) + 'px)';
 
 // button listener
 
-const start = button[0];
-const center = button[1];
-const end = button[2];
+const start = document.querySelector('.start');
+const center = document.querySelector('.center');
+const end = document.querySelector('.end');
+
+center.classList.add('active');
+center.classList.remove('bar');
 
 start.addEventListener('click', () =>{
-  if (counter <= 0) return; 
-    slide.style.transition = 'transform 0.4s ease-in-out';
-    counter--;
-    slide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+  slide.style.transition = 'transform 0.4s ease-in-out';
+  slide.style.transform = 'translateX(' + (-size * 0) + 'px)';
+
+  start.classList.add('active');
+  start.classList.remove('bar');
+  center.classList.add('bar');
+  end.classList.add('bar');
+});
+
+center.addEventListener('click', () =>{
+  if(slide.style.transform === 'translateX(' + (-size * 2) + 'px)'){
+    slide.style.transform = 'translateX(' + (-size * (1)) + 'px)';
+  } else if (slide.style.transform === 'translateX(' + (-size * 0) + 'px)'){
+    slide.style.transform = 'translateX(' + (-size * (1)) + 'px)';
+  } else {
+    return;
+  }
+
+  center.classList.add('active');
+  center.classList.remove('bar');
+  start.classList.add('bar');
+  end.classList.add('bar');
 });
 
 end.addEventListener('click', () =>{
-  if (counter >= card.length - 1) return; 
-    slide.style.transition = 'transform 0.4s ease-in-out';
-    counter++;
-    slide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+  slide.style.transition = 'transform 0.4s ease-in-out';
+  slide.style.transform = 'translateX(' + (-size * 2) + 'px)';
+
+  end.classList.add('active');
+  end.classList.remove('bar');
+  start.classList.add('bar');
+  center.classList.add('bar');
+  
 });
+
 
